4abe56a6e9abec1056883f127557ab4a8d14b750
leesangyeop <infightersy@naver.com>
2022-11-15 09:55:45 +0900

    Initial commit

---
+++ src/main/java/study/hierarchical_project1/HierarchicalProject1Application.java
@@ -0,0 +1,13 @@
+package study.hierarchical_project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HierarchicalProject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HierarchicalProject1Application.class, args);
+    }
+
+}
---
+++ src/test/java/study/hierarchical_project1/HierarchicalProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.hierarchical_project1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HierarchicalProject1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
